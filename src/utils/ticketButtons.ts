bb16ee572073a10c1bd7e7cdb51452d6bb780235
Neo <neodevils_contact@icloud.com>
2025-07-30 19:48:27 +0300

    feat: ticket buttons

---
+++ src/utils/ticketButtons.ts
@@ -0,0 +1,20 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
+import { emojis } from "@utils/emojis.js";
+
+const lockButton = new ButtonBuilder()
+	.setCustomId("ticket-lock-conversation")
+	.setLabel("Lock Ticket")
+	.setStyle(ButtonStyle.Secondary)
+	.setDisabled(false)
+	.setEmoji(emojis.ticket.bubble.lock);
+
+const karuButton = new ButtonBuilder()
+	.setCustomId("karu-button")
+	.setLabel("Kāru AI (Beta)")
+	.setStyle(ButtonStyle.Secondary)
+	.setDisabled(false)
+	.setEmoji(emojis.intelligence);
+
+const ticketButtonRow = new ActionRowBuilder().addComponents(lockButton, karuButton);
+
+export { ticketButtonRow };
